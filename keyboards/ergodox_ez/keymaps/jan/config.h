c1eb926a1a85a793917309c2b328956117f88aa6
Jan Varwig <jan@varwig.org>
2017-11-18 14:11:31 +0100

    jan

---
+++ keyboards/ergodox_ez/keymaps/jan/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+#include "../../config.h"
+
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          50
+#define MOUSEKEY_TIME_TO_MAX    15
+#define MOUSEKEY_MAX_SPEED       3
+#define MOUSEKEY_WHEEL_DELAY 0
+
+#endif
